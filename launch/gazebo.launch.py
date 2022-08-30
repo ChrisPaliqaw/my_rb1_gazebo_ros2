8c867cadd684930f3660cf1b7e7eac5f542ff0d6
Chris Paliqaw <christophomos@gmail.com>
2022-08-30 17:19:39 +0000

    feat: Spawn rb1 in gazebo empty world

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -2,8 +2,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
 
 # this is the function launch  system will look for
 def generate_launch_description():
@@ -12,6 +16,33 @@ def generate_launch_description():
     urdf_file = 'my_rb1_robot.urdf'
     #xacro_file = "box_bot.xacro"
     package_description = "my_rb1_description"
+    install_dir = get_package_prefix(package_description)
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot_gazebo = get_package_share_directory('my_rb1_gazebo')
+
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    ) 
 
     ####### DATA INPUT END ##########
     print("Fetching URDF ==>")
@@ -43,6 +74,11 @@ def generate_launch_description():
     return LaunchDescription(
         [            
             robot_state_publisher_node,
-            rviz_node
+            rviz_node,
+            DeclareLaunchArgument(
+                'world',
+                default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'empty.world'), ''],
+                description='SDF world file'),
+            gazebo
         ]
     )
